f6d0ac539759d5aaa1e1594196dd3ffb2ff9c923
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-10-01 13:32:46 -0400

    foo

--- src/miniscaffold-test-local-paket/AssemblyInfo.fs
+++ src/miniscaffold-test-local-paket/AssemblyInfo.fs
@@ -9,7 +9,7 @@ open System.Reflection
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","bb96618579ca6ea81e4bca11892be616ab4e9ea4")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -20,4 +20,4 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "bb96618579ca6ea81e4bca11892be616ab4e9ea4"
